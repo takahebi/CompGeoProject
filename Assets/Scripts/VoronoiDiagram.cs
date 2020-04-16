fda80b4f642c82c10badbb729e60e5a8a80bc7ba
takahebi <lkuivila@gmail.com>
2020-04-16 14:52:58 -0400

    working
    working base implimentation of the library

--- Assets/Scripts/VoronoiDiagram.cs
+++ Assets/Scripts/VoronoiDiagram.cs
@@ -1,103 +1,107 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
+
+using csDelaunay;
 
 public class VoronoiDiagram : MonoBehaviour
 {
-	public Vector2Int imageDim;
-	public int regionAmount;
-	public bool drawByDistance = false;
-	private void Start()
-	{
-		GetComponent<SpriteRenderer>().sprite = Sprite.Create((drawByDistance ? GetDiagramByDistance() 
-            : GetDiagram()), new Rect(0, 0, imageDim.x, imageDim.y), Vector2.one * 0.5f);
-	}
-	Texture2D GetDiagram()
-	{
-		Vector2Int[] centroids = new Vector2Int[regionAmount];
-		Color[] regions = new Color[regionAmount];
-		for(int i = 0; i < regionAmount; i++)
-		{
-			centroids[i] = new Vector2Int(Random.Range(0, imageDim.x), Random.Range(0, imageDim.y));
-			regions[i] = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
-		}
-		Color[] pixelColors = new Color[imageDim.x * imageDim.y];
-		for(int x = 0; x < imageDim.x; x++)
-		{
-			for(int y = 0; y < imageDim.y; y++)
-			{
-				int index = x * imageDim.x + y;
-				pixelColors[index] = regions[GetClosestCentroidIndex(new Vector2Int(x, y), centroids)];
-			}
-		}
-		return GetImageFromColorArray(pixelColors);
-	}
-	Texture2D GetDiagramByDistance()
-	{
-		Vector2Int[] centroids = new Vector2Int[regionAmount];
-		
-		for (int i = 0; i < regionAmount; i++)
-		{
-			centroids[i] = new Vector2Int(Random.Range(0, imageDim.x), Random.Range(0, imageDim.y));
-		}
-		Color[] pixelColors = new Color[imageDim.x * imageDim.y];
-		float[] distances = new float[imageDim.x * imageDim.y];
 
-		//you can get the max distance in the same pass as you calculate the distances. :P oops!
-		float maxDst = float.MinValue;
-		for (int x = 0; x < imageDim.x; x++)
-		{
-			for (int y = 0; y < imageDim.y; y++)
-			{
-				int index = x * imageDim.x + y;
-				distances[index] = Vector2.Distance(new Vector2Int(x,y), centroids[GetClosestCentroidIndex(new Vector2Int(x,y), centroids)]);
-				if(distances[index] > maxDst)
-				{
-					maxDst = distances[index];
-				}
-			}	
-		}
+    // The number of polygons/sites we want
+    public int polygonNumber = 200;
 
-		for(int i = 0; i < distances.Length; i++)
-		{
-			float colorValue = distances[i] / maxDst;
-			pixelColors[i] = new Color(colorValue, colorValue, colorValue, 1f);
-		}
-		return GetImageFromColorArray(pixelColors);
-	}
-	/* didn't actually need this
-	float GetMaxDistance(float[] distances)
-	{
-		float maxDst = float.MinValue;
-		for(int i = 0; i < distances.Length; i++)
-		{
-			if(distances[i] > maxDst)
-			{
-				maxDst = distances[i];
-			}
-		}
-		return maxDst;
-	}*/
-	int GetClosestCentroidIndex(Vector2Int pixelPos, Vector2Int[] centroids)
-	{
-		float smallestDst = float.MaxValue;
-		int index = 0;
-		for(int i = 0; i < centroids.Length; i++)
-		{
-			if (Vector2.Distance(pixelPos, centroids[i]) < smallestDst)
-			{
-				smallestDst = Vector2.Distance(pixelPos, centroids[i]);
-				index = i;
-			}
-		}
-		return index;
-	}
-	Texture2D GetImageFromColorArray(Color[] pixelColors)
-	{
-		Texture2D tex = new Texture2D(imageDim.x, imageDim.y);
-		tex.filterMode = FilterMode.Point;
-		tex.SetPixels(pixelColors);
-		tex.Apply();
-		return tex;
-	}
+    // This is where we will store the resulting data
+    private Dictionary<Vector2f, Site> sites;
+    private List<Edge> edges;
+
+    void Start()
+    {
+        // Create your sites (lets call that the center of your polygons)
+        List<Vector2f> points = CreateRandomPoint();
+
+        // Create the bounds of the voronoi diagram
+        // Use Rectf instead of Rect; it's a struct just like Rect and does pretty much the same,
+        // but like that it allows you to run the delaunay library outside of unity (which mean also in another tread)
+        Rectf bounds = new Rectf(0, 0, 512, 512);
+
+        // There is a two ways you can create the voronoi diagram: with or without the lloyd relaxation
+        // Here I used it with 2 iterations of the lloyd relaxation
+        Voronoi voronoi = new Voronoi(points, bounds, 5);
+
+        // But you could also create it without lloyd relaxtion and call that function later if you want
+        //Voronoi voronoi = new Voronoi(points,bounds);
+        //voronoi.LloydRelaxation(5);
+
+        // Now retreive the edges from it, and the new sites position if you used lloyd relaxtion
+        sites = voronoi.SitesIndexedByLocation;
+        edges = voronoi.Edges;
+
+        DisplayVoronoiDiagram();
+    }
+
+    private List<Vector2f> CreateRandomPoint()
+    {
+        // Use Vector2f, instead of Vector2
+        // Vector2f is pretty much the same than Vector2, but like you could run Voronoi in another thread
+        List<Vector2f> points = new List<Vector2f>();
+        for (int i = 0; i < polygonNumber; i++)
+        {
+            points.Add(new Vector2f(Random.Range(0, 512), Random.Range(0, 512)));
+        }
+
+        return points;
+    }
+
+    // Here is a very simple way to display the result using a simple bresenham line algorithm
+    // Just attach this script to a quad
+    private void DisplayVoronoiDiagram()
+    {
+        Texture2D tx = new Texture2D(512, 512);
+        foreach (KeyValuePair<Vector2f, Site> kv in sites)
+        {
+            tx.SetPixel((int)kv.Key.x, (int)kv.Key.y, Color.red);
+        }
+        foreach (Edge edge in edges)
+        {
+            // if the edge doesn't have clippedEnds, if was not within the bounds, dont draw it
+            if (edge.ClippedEnds == null) continue;
+
+            DrawLine(edge.ClippedEnds[LR.LEFT], edge.ClippedEnds[LR.RIGHT], tx, Color.black);
+        }
+        tx.Apply();
+
+        this.GetComponent<Renderer>().material.mainTexture = tx;
+    }
+
+    // Bresenham line algorithm
+    private void DrawLine(Vector2f p0, Vector2f p1, Texture2D tx, Color c, int offset = 0)
+    {
+        int x0 = (int)p0.x;
+        int y0 = (int)p0.y;
+        int x1 = (int)p1.x;
+        int y1 = (int)p1.y;
+
+        int dx = Mathf.Abs(x1 - x0);
+        int dy = Mathf.Abs(y1 - y0);
+        int sx = x0 < x1 ? 1 : -1;
+        int sy = y0 < y1 ? 1 : -1;
+        int err = dx - dy;
+
+        while (true)
+        {
+            tx.SetPixel(x0 + offset, y0 + offset, c);
+
+            if (x0 == x1 && y0 == y1) break;
+            int e2 = 2 * err;
+            if (e2 > -dy)
+            {
+                err -= dy;
+                x0 += sx;
+            }
+            if (e2 < dx)
+            {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    }
 }
